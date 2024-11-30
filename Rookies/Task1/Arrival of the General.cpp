a2d1b3c335421eaec4e8d9d40eebe36523bd4b52
ShaFeiii <elshafeim45@gmail.com>
2024-11-30 02:12:57 +0200

    Rookies

---
+++ Rookies/Task1/Arrival of the General.cpp
@@ -0,0 +1,83 @@
+//      ﷽
+
+//       وَأَنْ لَيْسَ لِلْإِنْسَانِ إِلَّا مَا سَعَى
+
+///     However long the night ,the dawn will break.
+///     ACPC Next Year, ISA
+
+//#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+#define Wl3_ElNor       ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define i64             long long
+#define int             long long
+#define all(a)          a.begin(),a.end()
+#define all_r(a)        a.rbegin(),a.rend()
+#define vi              vector<int>
+#define ef              else if
+#define retv(x)         return void(cout << (x) << '\n')
+#define sp              " "
+#define SetPre(n, x)    cout << fixed << setprecision(n) << x << '\n'
+#define Mod             1'000'000'007
+#define INF             2'000'000'000
+#define sz(s)           (int)(s).size()
+#define debug(x)        cout << #x << ": " << (x) << "\n";
+#define EPS             1e-9
+#define PI              acos(-1)
+
+//const int dx[] = {0, 0, 1, -1};
+//const int dy[] = {1, -1, 0, 0};
+//const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//const int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+//const int N = 2e5+ 5;
+
+void Erz3() {
+    int n, moves = 0;
+    cin >> n;
+    vi soldiers(n);
+    for (auto& i : soldiers) cin >> i;
+    pair<int,int> minPos(INF, INF), maxPos(-INF, INF);
+    {
+        // get the max soldier position
+        for (int i = n - 1 ; i >= 0 ; --i) {
+            if (maxPos.first <= soldiers[i]) {
+                maxPos.first = soldiers[i];
+                maxPos.second = i;
+            }
+        }
+    }
+    {
+        // get the min soldier position
+        for (int i = 0 ; i < n ; ++i) {
+            if (minPos.first >= soldiers[i]) {
+                minPos.first = soldiers[i];
+                minPos.second = i;
+            }
+        }
+    }
+    moves += (maxPos.first != -INF ? maxPos.second - 1: 0);
+    moves += (minPos.first != INF ? n - minPos.second : 0);
+    if (maxPos.second > minPos.second) moves--;
+    cout << moves;
+}
+
+signed main() {
+    Wl3_ElNor
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+//    cin >> t;
+    while (t--)
+        Erz3();
+
+    return 0;
+}
