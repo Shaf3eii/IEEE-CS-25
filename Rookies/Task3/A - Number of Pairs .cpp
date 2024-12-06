e6b5539b280024b952a530f43c08588d78896bca
ShaFeiii <elshafeim45@gmail.com>
2024-12-06 21:20:32 +0200

    Task3 Done

---
+++ Rookies/Task3/A - Number of Pairs .cpp
@@ -0,0 +1,77 @@
+//      ﷽
+
+//       وَأَنْ لَيْسَ لِلْإِنْسَانِ إِلَّا مَا سَعَى
+
+//       فَالجُهدُ يُثمِرُ إنْ تَضافَرَ صَفوُهُ، والعَزمُ يَرفعُ صَرحَ كُلِّ بُنيانِ.
+
+///     However long the night ,the dawn will break.
+///     ACPC Next Year, ISA
+
+//#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+#define Wl3_ElNor       ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define i64             long long
+#define int             long long
+#define all(a)          a.begin(),a.end()
+#define all_r(a)        a.rbegin(),a.rend()
+#define vi              vector<int>
+#define ef              else if
+#define retv(x)         return void(cout << (x) << '\n')
+#define sp              " "
+#define SetPre(n, x)    cout << fixed << setprecision(n) << x << '\n'
+#define Mod             1'000'000'007
+#define INF             2'000'000'000
+#define sz(s)           (int)(s).size()
+#define debug(x)        cout << #x << ": " << (x) << "\n";
+#define EPS             1e-9
+#define PI              acos(-1)
+
+//const int dx[] = {0, 0, 1, -1};
+//const int dy[] = {1, -1, 0, 0};
+//const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//const int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+//const int N = 1e5+ 5;
+
+void Erz3() {
+    int size, lowerSum, upperSum;
+    cin >> size >> lowerSum >> upperSum;
+
+    vi nums(size);
+    for (auto& i : nums) cin >> i;
+
+    sort(all(nums));
+
+    int lowerSumPointer = size, upperSumPointer = size, pairs = 0;
+    for (int i = 0; i < size; ++i) {
+        while (lowerSumPointer and nums[lowerSumPointer - 1] + nums[i] >= lowerSum)
+            lowerSumPointer--;
+
+        while (upperSumPointer and nums[upperSumPointer - 1] + nums[i] > upperSum)
+            upperSumPointer--;
+
+        pairs += min(upperSumPointer, i) - min(lowerSumPointer, i);
+    }
+    cout << pairs << '\n';
+}
+
+signed main() {
+    Wl3_ElNor
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+        Erz3();
+
+    return 0;
+}
